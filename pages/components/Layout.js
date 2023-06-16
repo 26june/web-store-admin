dc6611f647a5f402b836ad183b150efef4325756
26june <gj.abordo1@gmail.com>
2023-06-16 21:52:30 +0100

    refactor layout

---
+++ pages/components/Layout.js
@@ -0,0 +1,29 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+import Nav from "./Nav";
+
+export default function Layout({ children }) {
+  const { data: session } = useSession();
+  if (!session) {
+    return (
+      <div className="bg-red-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button
+            className="bg-white p-2 px-4 rounded-lg"
+            onClick={() => signIn("google")}
+          >
+            Login With Google
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="bg-blue-900 min-h-screen flex">
+      <Nav></Nav>
+      <div className="bg-white flex-grow p-4 mt-2 mr-2 mb-2 rounded-lg">
+        {children}
+      </div>
+    </div>
+  );
+}
