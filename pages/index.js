eb28faaaa66161e4149e36e0bd02eee49ee6c65d
26june <gj.abordo1@gmail.com>
2023-06-16 21:53:12 +0100

    refactor index

--- pages/index.js
+++ pages/index.js
@@ -3,38 +3,8 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-import { useSession, signIn, signOut } from "next-auth/react";
-import Nav from "./components/Nav";
+import Layout from "./components/Layout";
 
 export default function Home() {
-  const { data: session } = useSession();
-  if (!session) {
-    return (
-      <div className="bg-red-900 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button
-            className="bg-white p-2 px-4 rounded-lg"
-            onClick={() => signIn("google")}
-          >
-            Login With Google
-          </button>
-        </div>
-      </div>
-    );
-  }
-
-  return (
-    <div className="bg-blue-900 min-h-screen">
-      <Nav></Nav>
-      <div>Logged in as {session.user.name}</div>
-      <div>
-        <button
-          className="bg-white p-2 px-4 rounded-lg"
-          onClick={() => signOut()}
-        >
-          Sign Out
-        </button>
-      </div>
-    </div>
-  );
+  return <Layout>Home / Dashboard</Layout>;
 }
