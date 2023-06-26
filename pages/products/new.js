30dc20989cacddefc455611886ee16a433d96a0e
26june <gj.abordo1@gmail.com>
2023-06-26 20:33:55 +0100

    create form for new products

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,16 +1,41 @@
+import { useState } from "react";
 import Layout from "../components/Layout";
 
 export default function NewProduct() {
+  const [productName, setProductName] = useState("");
+  const [productDescription, setProductDescription] = useState("");
+  const [productPrice, setProductPrice] = useState("");
+
+  function createProduct() {}
+
   return (
     <Layout>
-      <h1>New Product</h1>
-      <label>Product Name</label>
-      <input type="text" placeholder="Product Name"></input>
-      <label>Description</label>
-      <textarea placeholder="Description..."></textarea>
-      <label>Price</label>
-      <input type="number" placeholder="Price"></input>
-      <button className="btn-primary">Save</button>
+      <form onSubmit={createProduct}>
+        <h1>New Product</h1>
+        <label>Product Name</label>
+        <input
+          type="text"
+          placeholder="Product Name"
+          value={productName}
+          onChange={(e) => setProductName(e.target.value)}
+        ></input>
+        <label>Description</label>
+        <textarea
+          placeholder="Description..."
+          value={productDescription}
+          onChange={(e) => setProductDescription(e.target.value)}
+        ></textarea>
+        <label>Price (GBP)</label>
+        <input
+          type="number"
+          placeholder="Price"
+          value={productPrice}
+          onChange={(e) => setProductPrice(e.target.value)}
+        ></input>
+        <button type="submit" className="btn-primary">
+          Save
+        </button>
+      </form>
     </Layout>
   );
 }
